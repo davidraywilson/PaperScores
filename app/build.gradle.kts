e8ce512551154c71cab252b06036f03153e79e9a
David Wilson <davidray.real@gmail.com>
2026-07-21 20:04:33 -0500

    Release candidate 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.paperapps.paperscores"
         minSdk = 28
         targetSdk = 36
-        versionCode = 44
-        versionName = "0.0.44"
+        versionCode = 48
+        versionName = "0.0.48"
     }
 
     buildTypes {
